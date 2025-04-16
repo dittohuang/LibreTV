d0b91edcf8092e09d8c8fadbc2c47068e69a49bd
Tao Huang <tao.huang@teknique.com>
2025-04-16 22:17:53 +0800

    update resources

--- js/config.js
+++ js/config.js
@@ -17,7 +17,7 @@ const SITE_CONFIG = {
 // API站点配置
 const API_SITES = {
     heimuer: {
-        api: 'https://json.heimuer.xyz',
+        api: 'https://json02.heimuer.xyz',
         name: '黑木耳',
         detail: 'https://heimuer.tv'
     },
@@ -30,11 +30,6 @@ const API_SITES = {
         api: 'https://tyyszy.com',
         name: '天涯资源',
     },
-    ckzy: {
-        api: 'https://www.ckzy1.com',
-        name: 'CK资源',
-        adult: true
-    },
     zy360: {
         api: 'https://360zy.com',
         name: '360资源',
@@ -45,11 +40,7 @@ const API_SITES = {
     },
     cjhw: {
         api: 'https://cjhwba.com',
-        name: '新华为',
-    },
-    hwba: {
-        api: 'https://cjwba.com',
-        name: '华为吧资源',
+        name: '华为吧',
     },
     jisu: {
         api: 'https://jszyapi.com',
@@ -60,10 +51,6 @@ const API_SITES = {
         api: 'https://dbzy.com',
         name: '豆瓣资源',
     },
-    bfzy: {
-        api: 'https://bfzyapi.com',
-        name: '暴风资源',
-    },
     mozhua: {
         api: 'https://mozhuazy.com',
         name: '魔爪资源',
@@ -71,47 +58,6 @@ const API_SITES = {
     mdzy: {
         api: 'https://www.mdzyapi.com',
         name: '魔都资源',
-    },
-    ruyi: {
-        api: 'https://cj.rycjapi.com',
-        name: '如意资源',
-    },
-    
-    jkun: {
-        api: 'https://jkunzyapi.com',
-        name: 'jkun资源',
-        adult: true
-    },
-    bwzy: {
-        api: 'https://api.bwzym3u8.com',
-        name: '百万资源',
-        adult: true
-    },
-    souav: {
-        api: 'https://api.souavzy.vip',
-        name: 'souav资源',
-        adult: true
-    },
-    siwa: {
-        api: 'https://siwazyw.tv',
-        name: '丝袜资源',
-        adult: true
-    },
-    r155: {
-        api: 'https://155api.com',
-        name: '155资源',
-        adult: true
-    },
-    lsb: {
-        api: 'https://apilsbzy1.com',
-        name: 'lsb资源',
-        adult: true
-    },
-    huangcang: {
-        api: 'https://hsckzy.vip',
-        name: '黄色仓库',
-        adult: true,
-        detail: 'https://hsckzy.vip' // 添加detail URL以便特殊处理
     }
 };
 
